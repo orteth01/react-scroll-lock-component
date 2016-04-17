4b0d0d7a534bcbc576917dbc8473221734d20282
Teddy Ortega <tortega128@gmail.com>
2016-04-16 22:47:15 -0400

    initial component src and build

---
+++ src/ScrollLock.jsx
@@ -0,0 +1,50 @@
+import React, { Component, PropTypes } from 'react';
+
+class ScrollLock extends Component {
+
+  static propTypes = {
+    children: PropTypes.array.isRequired
+  };
+
+  componentDidMount() {
+    this.scrollingElement = this.refs.scrollLock.firstChild;
+    this.scrollingElement.addEventListener('wheel', this.onScrollHandler, false);
+  };
+
+  componentWillUnmount() {
+    this.scrollingElement.removeEventListener('wheel', this.onScrollHandler, false);
+  };
+
+  onScrollHandler = (e) => {
+    const elem = this.scrollingElement;
+    const { scrollTop, scrollHeight, clientHeight } = elem;
+    const wheelDelta = e.deltaY;
+    const isDeltaPositive = wheelDelta > 0;
+
+    if (isDeltaPositive && wheelDelta > scrollHeight - clientHeight - scrollTop) {
+      elem.scrollTop = scrollHeight;
+      return this.cancelScrollEvent(e);
+    }
+    else if (!isDeltaPositive && -wheelDelta > scrollTop) {
+      elem.scrollTop = 0;
+      return this.cancelScrollEvent(e);
+    }
+  };
+
+  cancelScrollEvent = (e) => {
+    e.stopImmediatePropagation();
+    e.preventDefault();
+    e.returnValue = false;
+    return false;
+  };
+
+  render() {
+    return (
+      <div ref="scrollLock">
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default ScrollLock;

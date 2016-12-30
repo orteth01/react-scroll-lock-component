12741348a098809ae08d945348d14bd1211e906c
Teddy Ortega <tortega128@gmail.com>
2016-12-29 22:34:30 -0500

    add linting

--- demo/build/bundle.js
+++ demo/build/bundle.js
@@ -21637,17 +21637,17 @@
 	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 	Object.defineProperty(exports, "__esModule", {
-	  value: true
+	    value: true
 	});
 
 	var _createClass = function () {
-	  function defineProperties(target, props) {
-	    for (var i = 0; i < props.length; i++) {
-	      var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);
-	    }
-	  }return function (Constructor, protoProps, staticProps) {
-	    if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;
-	  };
+	    function defineProperties(target, props) {
+	        for (var i = 0; i < props.length; i++) {
+	            var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);
+	        }
+	    }return function (Constructor, protoProps, staticProps) {
+	        if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;
+	    };
 	}();
 
 	var _react = __webpack_require__(1);
@@ -21655,100 +21655,104 @@
 	var _react2 = _interopRequireDefault(_react);
 
 	function _interopRequireDefault(obj) {
-	  return obj && obj.__esModule ? obj : { default: obj };
+	    return obj && obj.__esModule ? obj : { default: obj };
 	}
 
 	function _classCallCheck(instance, Constructor) {
-	  if (!(instance instanceof Constructor)) {
-	    throw new TypeError("Cannot call a class as a function");
-	  }
+	    if (!(instance instanceof Constructor)) {
+	        throw new TypeError("Cannot call a class as a function");
+	    }
 	}
 
 	function _possibleConstructorReturn(self, call) {
-	  if (!self) {
-	    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-	  }return call && ((typeof call === "undefined" ? "undefined" : _typeof(call)) === "object" || typeof call === "function") ? call : self;
+	    if (!self) {
+	        throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+	    }return call && ((typeof call === "undefined" ? "undefined" : _typeof(call)) === "object" || typeof call === "function") ? call : self;
 	}
 
 	function _inherits(subClass, superClass) {
-	  if (typeof superClass !== "function" && superClass !== null) {
-	    throw new TypeError("Super expression must either be null or a function, not " + (typeof superClass === "undefined" ? "undefined" : _typeof(superClass)));
-	  }subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+	    if (typeof superClass !== "function" && superClass !== null) {
+	        throw new TypeError("Super expression must either be null or a function, not " + (typeof superClass === "undefined" ? "undefined" : _typeof(superClass)));
+	    }subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
 	}
 
 	var ScrollLock = function (_Component) {
-	  _inherits(ScrollLock, _Component);
+	    _inherits(ScrollLock, _Component);
 
-	  function ScrollLock() {
-	    var _ref;
+	    function ScrollLock() {
+	        var _ref;
 
-	    var _temp, _this, _ret;
+	        var _temp, _this, _ret;
 
-	    _classCallCheck(this, ScrollLock);
+	        _classCallCheck(this, ScrollLock);
 
-	    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-	      args[_key] = arguments[_key];
+	        for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+	            args[_key] = arguments[_key];
+	        }
+
+	        return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = ScrollLock.__proto__ || Object.getPrototypeOf(ScrollLock)).call.apply(_ref, [this].concat(args))), _this), _this.onScrollHandler = function (e) {
+	            var elem = _this.scrollingElement;
+	            var scrollTop = elem.scrollTop,
+	                scrollHeight = elem.scrollHeight,
+	                clientHeight = elem.clientHeight;
+
+	            var wheelDelta = e.deltaY;
+	            var isDeltaPositive = wheelDelta > 0;
+	            var shouldCancelScroll = false;
+
+	            if (isDeltaPositive && wheelDelta > scrollHeight - clientHeight - scrollTop) {
+	                elem.scrollTop = scrollHeight;
+	                shouldCancelScroll = true;
+	            } else if (!isDeltaPositive && -wheelDelta > scrollTop) {
+	                elem.scrollTop = 0;
+	                shouldCancelScroll = true;
+	            }
+
+	            if (shouldCancelScroll) {
+	                _this.cancelScrollEvent(e);
+	            }
+	        }, _this.setScrollingElement = function (r) {
+	            _this.scrollingElement = r ? r.firstChild : r;
+	        }, _this.cancelScrollEvent = function (e) {
+	            e.stopImmediatePropagation();
+	            e.preventDefault();
+	            return false;
+	        }, _temp), _possibleConstructorReturn(_this, _ret);
 	    }
 
-	    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = ScrollLock.__proto__ || Object.getPrototypeOf(ScrollLock)).call.apply(_ref, [this].concat(args))), _this), _this.setScrollingElement = function (r) {
-	      _this.scrollingElement = r ? r.firstChild : r;
-	    }, _this.onScrollHandler = function (e) {
-	      var elem = _this.scrollingElement;
-	      var scrollTop = elem.scrollTop,
-	          scrollHeight = elem.scrollHeight,
-	          clientHeight = elem.clientHeight;
+	    _createClass(ScrollLock, [{
+	        key: 'componentDidMount',
+	        value: function componentDidMount() {
+	            if (this.props.enabled) {
+	                this.scrollingElement.addEventListener('wheel', this.onScrollHandler, false);
+	            }
+	        }
+	    }, {
+	        key: 'componentWillReceiveProps',
+	        value: function componentWillReceiveProps(nextProps) {
+	            if (nextProps.enabled) {
+	                this.scrollingElement.addEventListener('wheel', this.onScrollHandler, false);
+	            } else {
+	                this.scrollingElement.removeEventListener('wheel', this.onScrollHandler, false);
+	            }
+	        }
+	    }, {
+	        key: 'componentWillUnmount',
+	        value: function componentWillUnmount() {
+	            this.scrollingElement.removeEventListener('wheel', this.onScrollHandler, false);
+	        }
+	    }, {
+	        key: 'render',
+	        value: function render() {
+	            return _react2.default.createElement('div', { ref: this.setScrollingElement }, this.props.children);
+	        }
+	    }]);
 
-	      var wheelDelta = e.deltaY;
-	      var isDeltaPositive = wheelDelta > 0;
-
-	      if (isDeltaPositive && wheelDelta > scrollHeight - clientHeight - scrollTop) {
-	        elem.scrollTop = scrollHeight;
-	        return _this.cancelScrollEvent(e);
-	      } else if (!isDeltaPositive && -wheelDelta > scrollTop) {
-	        elem.scrollTop = 0;
-	        return _this.cancelScrollEvent(e);
-	      }
-	    }, _this.cancelScrollEvent = function (e) {
-	      e.stopImmediatePropagation();
-	      e.preventDefault();
-	      e.returnValue = false;
-	      return false;
-	    }, _temp), _possibleConstructorReturn(_this, _ret);
-	  }
-
-	  _createClass(ScrollLock, [{
-	    key: 'componentDidMount',
-	    value: function componentDidMount() {
-	      if (this.props.enabled) {
-	        this.scrollingElement.addEventListener('wheel', this.onScrollHandler, false);
-	      }
-	    }
-	  }, {
-	    key: 'componentWillReceiveProps',
-	    value: function componentWillReceiveProps(nextProps) {
-	      if (nextProps.enabled) {
-	        this.scrollingElement.addEventListener('wheel', this.onScrollHandler, false);
-	      } else {
-	        this.scrollingElement.removeEventListener('wheel', this.onScrollHandler, false);
-	      }
-	    }
-	  }, {
-	    key: 'componentWillUnmount',
-	    value: function componentWillUnmount() {
-	      this.scrollingElement.removeEventListener('wheel', this.onScrollHandler, false);
-	    }
-	  }, {
-	    key: 'render',
-	    value: function render() {
-	      return _react2.default.createElement('div', { ref: this.setScrollingElement }, this.props.children);
-	    }
-	  }]);
-
-	  return ScrollLock;
+	    return ScrollLock;
 	}(_react.Component);
 
 	ScrollLock.defaultProps = {
-	  enabled: true
+	    enabled: true
 	};
 
 	exports.default = ScrollLock;
--- src/ScrollLock.jsx
+++ src/ScrollLock.jsx
@@ -1,62 +1,65 @@
 import React, { Component } from 'react';
 
 class ScrollLock extends Component {
-  componentDidMount() {
-    if (this.props.enabled) {
-      this.scrollingElement.addEventListener('wheel', this.onScrollHandler, false)
+    componentDidMount() {
+        if (this.props.enabled) {
+            this.scrollingElement.addEventListener('wheel', this.onScrollHandler, false);
+        }
     }
-  }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.enabled) {
-      this.scrollingElement.addEventListener('wheel', this.onScrollHandler, false)
-    } else {
-      this.scrollingElement.removeEventListener('wheel', this.onScrollHandler, false)
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.enabled) {
+            this.scrollingElement.addEventListener('wheel', this.onScrollHandler, false);
+        } else {
+            this.scrollingElement.removeEventListener('wheel', this.onScrollHandler, false);
+        }
     }
-  }
 
-  componentWillUnmount() {
-    this.scrollingElement.removeEventListener('wheel', this.onScrollHandler, false);
-  }
-
-  setScrollingElement = (r) => {
-    this.scrollingElement = r ? r.firstChild : r;
-  };
-
-  onScrollHandler = (e) => {
-    const elem = this.scrollingElement;
-    const { scrollTop, scrollHeight, clientHeight } = elem;
-    const wheelDelta = e.deltaY;
-    const isDeltaPositive = wheelDelta > 0;
-
-    if (isDeltaPositive && wheelDelta > scrollHeight - clientHeight - scrollTop) {
-      elem.scrollTop = scrollHeight;
-      return this.cancelScrollEvent(e);
+    componentWillUnmount() {
+        this.scrollingElement.removeEventListener('wheel', this.onScrollHandler, false);
     }
-    else if (!isDeltaPositive && -wheelDelta > scrollTop) {
-      elem.scrollTop = 0;
-      return this.cancelScrollEvent(e);
+
+    onScrollHandler = (e) => {
+        const elem = this.scrollingElement;
+        const { scrollTop, scrollHeight, clientHeight } = elem;
+        const wheelDelta = e.deltaY;
+        const isDeltaPositive = wheelDelta > 0;
+        let shouldCancelScroll = false;
+
+        if (isDeltaPositive && wheelDelta > scrollHeight - clientHeight - scrollTop) {
+            elem.scrollTop = scrollHeight;
+            shouldCancelScroll = true;
+        } else if (!isDeltaPositive && -wheelDelta > scrollTop) {
+            elem.scrollTop = 0;
+            shouldCancelScroll = true;
+        }
+
+        if (shouldCancelScroll) {
+            this.cancelScrollEvent(e);
+        }
+    };
+
+    setScrollingElement = (r) => {
+        this.scrollingElement = r ? r.firstChild : r;
+    };
+
+    cancelScrollEvent = (e) => {
+        e.stopImmediatePropagation();
+        e.preventDefault();
+        return false;
+    };
+
+    render() {
+        return (
+            <div ref={this.setScrollingElement}>
+                {this.props.children}
+            </div>
+        );
     }
-  };
-
-  cancelScrollEvent = (e) => {
-    e.stopImmediatePropagation();
-    e.preventDefault();
-    e.returnValue = false;
-    return false;
-  };
-
-  render() {
-    return (
-      <div ref={this.setScrollingElement}>
-        {this.props.children}
-      </div>
-    );
-  }
 }
 
 ScrollLock.defaultProps = {
-  enabled: true
+    enabled: true
 };
 
 export default ScrollLock;
